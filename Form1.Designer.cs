50f2e98b0c01a7509e9e3c45e0466de1a4316c62
rainz <91213275+rainz-extended@users.noreply.github.com>
2022-07-24 11:39:06 +0700

    update again

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,7 +53,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(430, 329);
+            this.ClientSize = new System.Drawing.Size(399, 329);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
