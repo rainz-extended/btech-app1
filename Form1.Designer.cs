ec07e38ae6c2c475cddab5ba8d828de7e22bbb51
rainz <91213275+rainz-extended@users.noreply.github.com>
2022-07-24 11:38:37 +0700

    update code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(371, 89);
+            this.textBox1.Location = new System.Drawing.Point(34, 106);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(229, 23);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(606, 89);
+            this.button1.Location = new System.Drawing.Point(269, 106);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -53,7 +53,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(430, 329);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
