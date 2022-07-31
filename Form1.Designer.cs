c67e1c2df5e18942126df85f669ddd02b3bd976c
rainz <91213275+rainz-extended@users.noreply.github.com>
2022-07-31 09:11:51 +0700

    message box adding

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.btnPlusNumber = new System.Windows.Forms.Button();
+            this.btnMinusNumber = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -49,11 +51,34 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnPlusNumber
+            // 
+            this.btnPlusNumber.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnPlusNumber.Location = new System.Drawing.Point(34, 197);
+            this.btnPlusNumber.Name = "btnPlusNumber";
+            this.btnPlusNumber.Size = new System.Drawing.Size(75, 58);
+            this.btnPlusNumber.TabIndex = 2;
+            this.btnPlusNumber.Text = "+";
+            this.btnPlusNumber.UseVisualStyleBackColor = true;
+            this.btnPlusNumber.Click += new System.EventHandler(this.btnPlusNumber_Click);
+            // 
+            // btnMinusNumber
+            // 
+            this.btnMinusNumber.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnMinusNumber.Location = new System.Drawing.Point(115, 197);
+            this.btnMinusNumber.Name = "btnMinusNumber";
+            this.btnMinusNumber.Size = new System.Drawing.Size(75, 58);
+            this.btnMinusNumber.TabIndex = 2;
+            this.btnMinusNumber.Text = "-";
+            this.btnMinusNumber.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(399, 329);
+            this.Controls.Add(this.btnMinusNumber);
+            this.Controls.Add(this.btnPlusNumber);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -67,5 +92,7 @@
 
         private TextBox textBox1;
         private Button button1;
+        private Button btnPlusNumber;
+        private Button btnMinusNumber;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -12,5 +12,21 @@ namespace btech_app1
             string text1 = textBox1.Text;
             MessageBox.Show(text1);
         }
+
+        private void btnPlusNumber_Click(object sender, EventArgs e)
+        {
+            string title = "Comfirm me";
+            string content = "Test a result content";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(content, title, buttons);
+
+            if (result == DialogResult.OK)
+            {
+                //หลังจากกด ok แล้วต้องการให้ทำไรต่อ
+            }
+            else { 
+                // เสร็จแล้ว
+            }
+        }
     }
 }
