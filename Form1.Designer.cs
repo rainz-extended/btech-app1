233d87a37942bc8bfd8ef0e7ba75ca047d809512
rainz <91213275+rainz-extended@users.noreply.github.com>
2022-07-31 13:16:46 +0700

    add messagebox

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,11 +53,52 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
             this.toolStripStatusLabel1.Text = "Software by Testing";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(137, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(137, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(218, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 45);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(137, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(156, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(396, 551);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.statusStrip1);
             this.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Name = "Form1";
@@ -70,5 +115,9 @@
 
         private StatusStrip statusStrip1;
         private ToolStripStatusLabel toolStripStatusLabel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,26 @@ namespace btech_app1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string content = "MR TIMO NIESSNER";
+            string title = "Say my name";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+
+            DialogResult resultbox = MessageBox.Show(content, title, buttons);
+
+            if (resultbox == DialogResult.Yes)
+            {
+                //ถ้าตอบใช่เกิดอะไรขึ้น
+                MessageBox.Show("ขอบคุณครับ", title, MessageBoxButtons.OK);
+            }
+            else 
+            {
+                //ถ้าตอบไม่ เกิดอะไรขึ้น
+                MessageBox.Show("ขออภัยนะครับ", title, MessageBoxButtons.OK);
+            }
+
+        }
     }
 }
