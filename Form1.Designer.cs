56abcb622c9d69cdf6f8cb0ea19ef5ef4526bf30
rainz <91213275+rainz-extended@users.noreply.github.com>
2022-07-31 11:49:08 +0700

    สร้างฟอร์มใหม่

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,66 +23,44 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.btnPlusNumber = new System.Windows.Forms.Button();
-            this.btnMinusNumber = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // statusStrip1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 106);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(229, 23);
-            this.textBox1.TabIndex = 0;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 529);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(396, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
             // 
-            // button1
+            // toolStripStatusLabel1
             // 
-            this.button1.Location = new System.Drawing.Point(269, 106);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnPlusNumber
-            // 
-            this.btnPlusNumber.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnPlusNumber.Location = new System.Drawing.Point(34, 197);
-            this.btnPlusNumber.Name = "btnPlusNumber";
-            this.btnPlusNumber.Size = new System.Drawing.Size(75, 58);
-            this.btnPlusNumber.TabIndex = 2;
-            this.btnPlusNumber.Text = "+";
-            this.btnPlusNumber.UseVisualStyleBackColor = true;
-            this.btnPlusNumber.Click += new System.EventHandler(this.btnPlusNumber_Click);
-            // 
-            // btnMinusNumber
-            // 
-            this.btnMinusNumber.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnMinusNumber.Location = new System.Drawing.Point(115, 197);
-            this.btnMinusNumber.Name = "btnMinusNumber";
-            this.btnMinusNumber.Size = new System.Drawing.Size(75, 58);
-            this.btnMinusNumber.TabIndex = 2;
-            this.btnMinusNumber.Text = "-";
-            this.btnMinusNumber.UseVisualStyleBackColor = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "Software by Testing";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 329);
-            this.Controls.Add(this.btnMinusNumber);
-            this.Controls.Add(this.btnPlusNumber);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(396, 551);
+            this.Controls.Add(this.statusStrip1);
+            this.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "B_Tech Application 1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,9 +68,7 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private Button button1;
-        private Button btnPlusNumber;
-        private Button btnMinusNumber;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace btech_app1
 {
     public partial class Form1 : Form
@@ -5,28 +15,6 @@ namespace btech_app1
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string text1 = textBox1.Text;
-            MessageBox.Show(text1);
-        }
-
-        private void btnPlusNumber_Click(object sender, EventArgs e)
-        {
-            string title = "Comfirm me";
-            string content = "Test a result content";
-            MessageBoxButtons buttons = MessageBoxButtons.OK;
-            DialogResult result = MessageBox.Show(content, title, buttons);
-
-            if (result == DialogResult.OK)
-            {
-                //หลังจากกด ok แล้วต้องการให้ทำไรต่อ
-            }
-            else { 
-                // เสร็จแล้ว
-            }
         }
     }
 }
